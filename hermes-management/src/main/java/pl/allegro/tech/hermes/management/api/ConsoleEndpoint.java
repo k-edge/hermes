2b5067eefa6d78084c037384cab6cd355f5371f8
Manish Katoch <manish.katoch@koerber.digital>
2021-02-01 14:32:35 +0100

    fixes health endpoint for management console.

--- hermes-management/src/main/java/pl/allegro/tech/hermes/management/api/ConsoleEndpoint.java
+++ hermes-management/src/main/java/pl/allegro/tech/hermes/management/api/ConsoleEndpoint.java
@@ -2,15 +2,17 @@ package pl.allegro.tech.hermes.management.api;
 
 import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
-import org.springframework.stereotype.Component;
-import pl.allegro.tech.hermes.management.domain.console.ConsoleService;
-
+import java.net.URI;
 import javax.ws.rs.GET;
 import javax.ws.rs.HttpMethod;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
-import java.net.URI;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthStatusHttpMapper;
+import org.springframework.boot.actuate.health.Status;
+import org.springframework.stereotype.Component;
+import pl.allegro.tech.hermes.management.domain.console.ConsoleService;
 
 @Component
 @Path("/")
@@ -21,6 +23,17 @@ public class ConsoleEndpoint {
 
     public ConsoleEndpoint(ConsoleService service) {
         this.service = service;
+    }
+
+    @GET
+    @Path("/health")
+    @Produces("application/json")
+    @ApiOperation(value = "Provides health check endpoint", httpMethod = HttpMethod.GET)
+    public Response healthCheck() {
+        HealthStatusHttpMapper mapper = new HealthStatusHttpMapper();
+        return Response.status(Response.Status.OK)
+            .entity(new Health.Builder().status(Status.UP).build())
+        .build();
     }
 
     @GET
