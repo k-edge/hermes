e9b41096c4a7248eecfa26567ddb08a7f9948011
Manish Katoch <manish.katoch@koerber.digital>
2021-02-01 14:55:34 +0100

    adds topic preview to frontend.

--- hermes-frontend/run-script.sh
+++ hermes-frontend/run-script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export HERMES_FRONTEND_OPTS="-Dzookeeper.connect.string=${MSK_ZK_CONNECT_STRING} -Dkafka.broker.list=${MSK_KAFKA_BROKER_LIST} -Dfrontend.port=8080"
+export HERMES_FRONTEND_OPTS="-Dzookeeper.connect.string=${MSK_ZK_CONNECT_STRING} -Dkafka.broker.list=${MSK_KAFKA_BROKER_LIST} -Dfrontend.port=8080 -Dfrontend.message.preview.enabled=true"
 export JAVA_OPTS="-Djava.security.egd=file:/dev/./urandom -XX:+UseG1GC -Xms1G -Xmx1G"
 /app/binary/bin/hermes-frontend
