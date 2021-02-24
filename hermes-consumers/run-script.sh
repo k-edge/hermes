37af204c563b84e498d513b91a4616fe202e37c6
Manish Katoch <manish.katoch@koerber.digital>
2021-02-24 18:31:59 +0530

    adds option to provide custom http port for consumer

--- hermes-consumers/run-script.sh
+++ hermes-consumers/run-script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export HERMES_CONSUMERS_OPTS="-Dzookeeper.connect.string=${MSK_ZK_CONNECT_STRING} -Dkafka.broker.list=${MSK_KAFKA_BROKER_LIST}"
+export HERMES_CONSUMERS_OPTS="-Dzookeeper.connect.string=${MSK_ZK_CONNECT_STRING} -Dkafka.broker.list=${MSK_KAFKA_BROKER_LIST} -Dconsumer.status.health.port=${HERMES_CONSUMER_HTTP_PORT}"
 export JAVA_OPTS="-Djava.security.egd=file:/dev/./urandom -XX:+UseG1GC -Xms1G -Xmx1G"
 /app/binary/bin/hermes-consumers
