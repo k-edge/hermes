7e48f29903975f7a61d6de56030105a7ff4e5732
Manish Katoch <manish.katoch@koerber.digital>
2021-03-30 21:28:25 +0200

    allow removals from management.

--- hermes-management/run-script.sh
+++ hermes-management/run-script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 export HERMES_MANAGEMENT_OPTS="-Dzookeeper.connect.string=${MSK_ZK_CONNECT_STRING} -Dkafka.broker.list=${MSK_KAFKA_BROKER_LIST} -Dserver.port=8080"
-export JAVA_OPTS="-Djava.security.egd=file:/dev/./urandom -Xms1G -Xmx1G"
+export JAVA_OPTS="-Djava.security.egd=file:/dev/./urandom -XX:+UseG1GC -Xms1g -Xmx1G"
 /app/binary/bin/hermes-management
