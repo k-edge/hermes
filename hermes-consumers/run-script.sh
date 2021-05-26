b9f93d999c88a5d705d342d29f0a50b256639a59
Wouter Westenbrink <wouter@westenbrink.de>
2021-05-26 16:43:56 +0200

    remove unnecessary param

--- hermes-consumers/run-script.sh
+++ hermes-consumers/run-script.sh
@@ -2,7 +2,7 @@
 
 export HERMES_CONSUMERS_OPTS="-Dzookeeper.connect.string=${MSK_ZK_CONNECT_STRING} \
   -Dkafka.broker.list=${MSK_KAFKA_BROKER_LIST} \
-  -Dconsumer.status.health.port=${HERMES_CONSUMER_HTTP_PORT} \
+  -Dconsumer.status.health.port=8080 \
   -Dschema.repository.serverUrl=${KAFKA_SCHEMA_REGISTRY_URL} \
   -Dschema.repository.subject.suffix.enabled=true"
 export JAVA_OPTS="-Djava.security.egd=file:/dev/./urandom -XX:+UseG1GC -Xms5G -Xmx5G"
