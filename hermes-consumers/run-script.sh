1f10e2cf14a5002aa4449f6decc52007d9bb08ad
Wouter Westenbrink <wouter@westenbrink.de>
2021-05-26 16:58:29 +0200

    revert to 3GB of heap for hermes consumers

--- hermes-consumers/run-script.sh
+++ hermes-consumers/run-script.sh
@@ -5,5 +5,5 @@ export HERMES_CONSUMERS_OPTS="-Dzookeeper.connect.string=${MSK_ZK_CONNECT_STRING
   -Dconsumer.status.health.port=8080 \
   -Dschema.repository.serverUrl=${KAFKA_SCHEMA_REGISTRY_URL} \
   -Dschema.repository.subject.suffix.enabled=true"
-export JAVA_OPTS="-Djava.security.egd=file:/dev/./urandom -XX:+UseG1GC -Xms5G -Xmx5G"
+export JAVA_OPTS="-Djava.security.egd=file:/dev/./urandom -XX:+UseG1GC -Xms3G -Xmx3G"
 /app/binary/bin/hermes-consumers
